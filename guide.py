a6679236209085732f8fc3b66d97ba2936c3c9f6
yyongjae <dydwo706@naver.com>
2023-11-27 01:49:55 +0900

    feat: 승강장 찾기 flow 1차 완성
    - guide파일에서 start함수 -> board함수

---
+++ guide.py
@@ -0,0 +1,279 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+from dotenv import load_dotenv
+import json
+import time
+import uuid
+import requests
+import io
+import base64
+from PIL import Image
+from similarity import find_same_string, spacy_similarities, jaccard_similarity, Levenshtein_similarity
+
+load_dotenv()
+
+class Guide:
+    def __init__(self, img, box_info, situation='board'):
+        self.img = img
+        self.box_info = box_info
+        self.situation = situation # 1.길찾기  2.화장실찾기  3.나가기
+        
+        self.ocr_key = os.getenv("OCR_KEY")
+        self.ocr_url = os.getenv("OCR_URL")
+        
+        self.guide_board = list()
+        self.arrow = list()
+        self.text = list()
+        
+    def get_text_center(self,box,feat):
+        
+        if feat == 'ocr':
+            x_coords = [vertex['x'] for vertex in box]
+            y_coords = [vertex['y'] for vertex in box]
+
+            center_x = sum(x_coords) / len(x_coords)
+            center_y = sum(y_coords) / len(y_coords)
+            
+        ### --- xyxy xywh 확인 필요 --- ###
+        elif feat == 'yolo':
+            x1, y1, x2, y2 = box
+            center_x = (x1 + x2) / 2
+            center_y = (y1 + y2) / 2
+            # x, y , w, h = box
+            # center_x = x + w//2
+            # center_y = y + h//2
+        
+        return center_x, center_y    
+    
+    def is_all_english(self, text):
+        # 영어만 있으면 true    
+        return all(char.isalpha() and ord(char) < 128 for char in text)
+
+    def is_all_korean(self,text):
+        # 한글만 있으면 True
+        for char in text:
+            if not ('\uAC00' <= char <= '\uD7A3'):
+                return False
+        return True
+
+
+    def to_byte_img(self, img):
+        # ocr 보내려면 바이트 형태의 이미지 값을 넘겨야 해서 변환.
+        numpy_image = np.array(img)
+        opencv_image = cv2.cvtColor(numpy_image, cv2.COLOR_RGB2BGR)
+        image_bytes = cv2.imencode('.jpg', opencv_image)[1].tobytes()
+
+        return image_bytes
+    
+    def process_ocr_data(self, data):
+        
+        processed_data = []
+        uid = data['images'][0]['uid']
+        # json 형태가 좀 복잡함. 아래의 iteration은 한 박스에 대한 정보가 들어있음.
+        for box in data['images'][0]['fields']:
+            
+            infer_text = box['inferText']
+
+            # 한글 외에 다른 문자 있으면 거르기
+            if not self.is_all_korean(infer_text):
+                continue
+            
+            # 중심 좌표 계산
+            bounding_poly = box['boundingPoly']['vertices']
+            center_x, center_y = self.get_text_center(bounding_poly, 'ocr')
+
+            processed_field = {'uid': uid, 'center': (center_x, center_y), 'text': infer_text}
+            processed_data.append(processed_field)
+
+        '''
+        [{'uid': 'd83c37b54b~',
+        'center': (532.5, 28.25),
+        'text': '고속터미널'}, ...]
+        '''
+        
+        return processed_data
+
+
+    def ocr(self):
+        
+        start = time.time()
+        img = self.img
+        
+        request_json = {
+            'images': [
+                {
+                    'format': 'jpg',
+                    'name': 'demo'
+                }
+            ],
+            'requestId': str(uuid.uuid4()),
+            'version': 'V2',
+            'timestamp': int(round(time.time() * 1000))
+        }
+        
+        payload = {'message': json.dumps(request_json).encode('UTF-8')}
+        
+        byte_img = self.to_byte_img(img)
+        files = [('file',byte_img)]
+        headers = {
+        'X-OCR-SECRET': self.ocr_key
+        }
+
+        response = requests.request("POST", self.ocr_url, headers=headers, data = payload, files = files)
+
+        result = response.text.encode('utf8')
+        result_json = json.loads(result.decode('utf8').replace("'", '"'))
+        
+        end = time.time()
+        delay = end - start
+        print(f"[OCR] 완료! 걸린 응답 시간: {delay}")
+        
+        processed_data = self.process_ocr_data(result_json)
+        
+        return processed_data
+    
+    def crop_boxes(self, img, gb_box, ar_box):
+        # 안드로이드에서 base64로 인코딩된 이미지 정보를 가져오게됨.
+        decoded_data = base64.b64decode(img)
+        image = Image.open(io.BytesIO(decoded_data))
+        g_x1, g_y1, g_x2, g_y2 = gb_box['box']
+        cropped_image = image.crop((g_x1, g_y1, g_x2, g_y2))
+        
+        # crop한 이미지에 맞게 화살표의 박스 좌표도 변환을 해준다.
+        for box in ar_box:
+            x1, y1, x2, y2 = box['box']
+            x1, y1, x2, y2 = x1 - g_x1, y1 - g_y1, x2 - g_x1, y2 - g_y1
+            box['box'] = [x1, y1, x2, y2]
+        
+        print(f'[CROP] 변환 완료')
+        
+        return cropped_image, ar_box
+    
+    def box_size(self, box):
+        ### --- box 좌표의 형태가 xyxy인지 xywh인지 확인 후 수정 필요 --- ###
+        x1, y1, x2, y2 = box
+        width = x2 - x1
+        height = y2 - y1
+        return width * height
+    
+    def distance(self, box1, box2):
+        # 박스 중심 좌표 두개 사이의 거리.
+        distance = ((box2[0] - box1[0])**2 + (box2[1] - box1[1])**2) ** 0.5
+        return distance
+    
+    def filtering_object(self,box_info):
+        
+        guide_board = []
+        arrows = []
+        outtlier = []
+        
+        ### --- 박스 정보에 따라 인덱싱 수정해야함 --- ###
+        for box in box_info:
+            
+            if box['label'] == 2: # guide board
+                guide_board.append(box)
+            elif box['label'] in [3,5,7,10,11]: # L, R, S, U, under
+                arrows.append(box)
+            else:
+                outtlier.append(box)
+                print(f"[Label 분류]{box['label']} 발견")
+        
+        # 안내판이 여러개 검출되었다면 가장 큰 녀석만 골라서 사용할 계획. -> 잘린 안내판이 있을 수 있기에 수정 필요.
+        board = guide_board[0]
+        
+        if len(guide_board) > 1:
+            max_size = 0
+            
+            for gb in guide_board:
+                size = self.box_size(gb['box'])
+                if max_size < size:
+                    max_size = size
+                    board = gb
+        
+        # 화살표가 모두 안내판 안에 있는 화살표인지 확인.
+        ### --- xyxy xywh 확인 --- ###
+        filtered_arrow = []
+        for arrow in arrows:
+            arrow_cen_x, arrow_cen_y = self.get_text_center(arrow['box'], 'yolo')
+            x1, y1, x2, y2 = board['box']
+            if (x1 <= arrow_cen_x <= x2) and (y1 <= arrow_cen_y <= y2):
+                filtered_arrow.append(arrow)
+            
+        print(f"[Filtering] 총 {len(filtered_arrow)}개의 화살표 인식")
+       
+        return board, filtered_arrow
+    
+    def mapping_arrow(self, ocr_data, arrow_data):
+        
+        result = {'arrows':[]}
+        
+        for text in ocr_data:
+            text_point = text['center']
+            direction = ''
+            max_distance = 0
+            for arrow in arrow_data:
+                arr_point = self.get_text_center(arrow['box'], 'yolo')
+                dist = self.distance(text_point, arr_point)
+                
+                if dist > max_distance:
+                    max_distance = dist
+                    direction = arrow['label']
+            result['arrows'].append({"type": direction,
+                                     "text": ocr_data['text']})
+        
+        return result
+            
+    def board(self,):
+        
+        box_info = self.box_info
+        org_img = self.img
+        # 화살표 있는지 체크하고 안내판, 화살표 리스트를 나눠서 받기.
+        ## 화살표가 안내판 박스 안에 있는지 체크
+        ### 해당 조건에 맞는 안내판 및 화살표 리스트 받아오기
+        guide_board, arrow = self.filtering_object(box_info)
+        
+        # crop한 안내판 이미지에 대해 OCR 진행
+        crop_g_board, trans_arrow_box = self.crop_boxes(org_img, guide_board, arrow)        
+        ocr_data = self.ocr(crop_g_board)
+        
+        for data in ocr_data:
+            text = data['text']
+            ## 텍스트 박스에 대해서 역 이름, 나가는 곳 등과 같은 정보만 필터링 및 단어 유사도 보완
+            sp, jc, lv = spacy_similarities(text), jaccard_similarity(text), Levenshtein_similarity(text)
+            mod_text = find_same_string(sp, jc, lv)
+            # --- 추가할 점 --- #
+            ## 유사도 엄청 낮으면 아예 빼버려야함. ## 
+            # 수정한 텍스트 반영
+            data['text'] = mod_text
+            if text != mod_text:
+                print(f'[텍스트 수정] 기존: {text} -> 수정: {mod_text}')
+        print('글자 수정 완료')
+        
+        # 화살표 박스와 텍스트 박스의 유클리드 거리를 비교하여 맵핑
+        result = self.mapping_arrow(ocr_data, trans_arrow_box)
+        
+        return result
+    
+    def toilet(self,):
+        pass
+    
+    def exit(self,):
+        pass
+            
+    def start(self,):
+        
+        # 상황에 따라 세가지로 나눠서 처리
+        ## 1. 길찾기
+        ## 2. 화장실찾기
+        ## 3. 출구찾기
+        if self.situation is 'board':
+            res = self.board()
+        elif self.situation is 'toilet':
+            self.toilet()
+        elif self.situation is 'exit':
+            self.exit()
+        
+        return res
+        
---
+++ similarity.py
@@ -0,0 +1,164 @@
+# -*- coding: utf-8 -*-
+
+
+# 필요한 것들 import
+#!pip install spacy
+import spacy
+#!python -m spacy download ko_core_news_sm
+nlp = spacy.load("ko_core_news_sm")
+import numpy as np
+import pandas as pd
+
+# 5호선, 7호선 지하철 역 이름 정리 리스트
+station_name_list = ['청구', '신금호', '행당', '왕십리', '마장', '답십리', '장한평', '군자', '아차산', '미사', '거여', '광나루', '천호', '강동', '길동', '굽은다리', '명일', '고덕', '상일동', '둔촌동', '올림픽공원', '방이', '오금', '개롱', '하남검단산', '하남시청', '하남풍산', '강일', '마천', '방화', '개화산', '김포공항', '송정', '마곡', '발산', '우장산', '화곡', '까치산', '신정', '목동', '오목교', '양평', '영등포구청', '영등포시장', '신길', '여의도', '여의나루', '마포', '공덕', '애오개', '충정로', '서대문', '광화문', '종로3가', '을지로4가', '동대문역사문화공원', '남성', '이수', '내방', '고속터미널', '반포', '논현', '학동', '강남구청', '청담', '뚝섬유원지', '건대입구', '어린이대공원', '군자', '중곡', '용마산', '산곡', '부평구청', '굴포천', '중화', '사가정', '면목', '상봉', '상동', '부천시청', '신중동', '춘의', '부천종합운동장', '까치울', '마들', '노원', '중계', '먹골', '하계', '공릉', '태릉입구', '석남', '삼산체육관', '수락산', '도봉산', '장암', '온수', '천왕', '광명사거리', '철산', '가산디지털단지', '남구로', '대림', '신풍', '보라매', '신대방삼거리', '장승배기', '상도', '숭실대입구']
+
+# 1. Spacy 활용
+# 2. 문자열 유사도 측정
+# - Levenshtein 거리 => 거리는 초성/중성/종성을 나눠서 진행
+# - Jaccard 유사성 지수
+
+# 초성 리스트. 00 ~ 18
+CHOSUNG_LIST = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+# 중성 리스트. 00 ~ 20
+JUNGSUNG_LIST = ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ']
+# 종성 리스트. 00 ~ 27 + 1(1개 없음)
+JONGSUNG_LIST = [' ', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+
+def korean_to(korean_word):
+    r_lst = []
+    for w in list(korean_word.strip()):
+        if '가'<=w<='힣':
+            ch1 = (ord(w) - ord('가'))//588
+            ch2 = ((ord(w) - ord('가')) - (588*ch1)) // 28
+            ch3 = (ord(w) - ord('가')) - (588*ch1) - 28*ch2
+            if(ch3 == 0):
+              r_lst.append([CHOSUNG_LIST[ch1], JUNGSUNG_LIST[ch2]])
+            else:
+              r_lst.append([CHOSUNG_LIST[ch1], JUNGSUNG_LIST[ch2], JONGSUNG_LIST[ch3]])
+        else:
+            r_lst.append([w])
+    return r_lst
+
+def levenshtein_distance(str1, str2):
+    # 문자열 길이 계산
+    row_len = len(str1) + 1
+    col_len = len(str2) + 1
+
+    # 빈 행렬 초기화, List comprehension 으로 2차 배열 선언쓰
+    matrix = [[0 for _ in range(col_len)] for _ in range(row_len)]
+
+    # 행렬 초기 설정
+    for row in range(row_len):
+        matrix[row][0] = row
+
+    for col in range(col_len):
+        matrix[0][col] = col
+
+    # Levenshtein 거리 계산
+    for i in range(1, row_len):
+        for j in range(1, col_len):
+            cost = 0 if str1[i - 1] == str2[j - 1] else 1
+            matrix[i][j] = min(
+                matrix[i - 1][j] + 1,      # 삭제
+                matrix[i][j - 1] + 1,      # 삽입
+                matrix[i - 1][j - 1] + cost  # 대체
+            )
+    return matrix[-1][-1]
+
+# 예제 사용
+# word1 = korean_to_be_englished("어린이대공원역")
+# word2 = korean_to_be_englished("어룬이공")
+
+# word1 = korean_word_to_initials("어린이대공원역")
+# word2 = korean_word_to_initials("어룬이공")
+
+word1 = korean_to("어린이대공원역")
+word2 = korean_to("어룬이공")
+
+print(word1)
+print(word2)
+
+distance = levenshtein_distance(word1, word2)
+print("Levenshtein 거리: {distance}")
+
+
+# 2. Levenshtein
+
+def Levenshtein_similarity(_ocr_test):
+    distances = {}
+    for station_name in real_station_names:
+      distance = levenshtein_distance(korean_to(_ocr_test), korean_to(station_name))
+      distances[station_name] = distance
+
+    # 유사성이 가장 높은 역 찾기
+    # most_similar_station = min(distances, key=distances.get)
+    # similarity_score = distances[most_similar_station]
+
+    # print("가장 유사한 역: ", most_similar_station)
+    # print("유사성 점수: ", similarity_score)
+    return min(distances, key=distances.get)
+
+
+
+# spaCy를 사용하여 유사성 측정
+
+def spacy_similarities(_ocr_test):
+  similarities = {}
+  for station_name in station_name_list:
+      doc1 = nlp(_ocr_test)
+      doc2 = nlp(station_name)
+      similarity = doc1.similarity(doc2)
+      similarities[station_name] = similarity
+  # 유사성이 가장 높은 역 찾기
+  return max(similarities, key=similarities.get)
+
+# Jaccard 유사도(Similarity)
+# 두 집합 간의 유사성을 측정하는 메트릭, 두 문자열을 각각 문자 단위로 나눈 후, 공통 문자의 비율 계산
+
+def jaccard(str1, str2):
+    # 두 문자열을 문자(또는 단어) 집합으로 변환
+    set1 = set(str1)
+    set2 = set(str2)
+
+    # 자카드 유사도 계산
+    intersection = len(set1.intersection(set2))
+    union = len(set1) + len(set2) - intersection
+    similarity = intersection / union
+
+    return similarity
+
+
+def jaccard_similarity(_ocr_test):
+    ja_similarities = {}
+    for station_name in real_station_names:
+        ja_similarity = jaccard(_ocr_test, station_name)
+        ja_similarities[station_name] = ja_similarity
+
+    # 유사성이 가장 높은 역 찾기
+    # most_similar_station = max(ja_similarities, key=ja_similarities.get)
+    # similarity_score = ja_similarities[most_similar_station]
+    
+    # print("가장 유사한 역: ", most_similar_station)
+    # print("유사성 점수: ", similarity_score)
+    return max(ja_similarities, key=ja_similarities.get)
+
+#################################################################
+
+# 아래의 변수에 확인하고자 하는 변수를 넣어주세요.
+ocr_test = "어른이대"
+
+def find_same_string(str1, str2, str3):
+    if str1 == str2 == str3:
+        return str1
+    elif str1 == str2:
+        return str1
+    elif str1 == str3:
+        return str1
+    elif str2 == str3:
+        return str2
+    else:
+        return str1
+
+# print(find_same_string(spacy_similarities(ocr_test), 
+#                  jaccard_similarity(ocr_test),
+#                  Levenshtein_similarity(ocr_test)))
